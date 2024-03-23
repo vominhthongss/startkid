f3c0309ceb7e5d4ae587c5ebbd1106504b6dbc34
vominhthongss <vominhthongss@gmail.com>
2024-03-23 10:49:13 +0900

    first commit

---
+++ readme.md
@@ -0,0 +1,15 @@
+1. Chạy lệnh
+   npm install
+   hoặc
+   yarn install
+2. Chạy app
+   \*Đối với ios
+   npm run ios
+
+   \*Đối với android
+   npm run androind
+
+3. Nếu dùng server tạm (mock data) thì (phải mở 1 terminal riêng)
+   cd server (lúc này bạn đang ở /server)
+   chạy lệnh
+   json-server --watch db.json --port 3000
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./src/screens/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+    "./src/navigations/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

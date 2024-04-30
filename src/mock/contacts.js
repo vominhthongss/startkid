e76d05da2884de9b8ed550d9d94b653fd0f300a7
Bao Le <davipboy123@gmail.com>
2024-04-30 11:55:08 +0700

    create contact screen

---
+++ src/mock/contacts.js
@@ -0,0 +1,49 @@
+
+
+export const contacts = [
+  {
+    contactCaterogy: "Liên hệ Nhà trường - Giáo viên",
+    contactItems: [
+      {
+        contactName: "Nhà trường",
+        phoneNumber: "0988938139",
+        avatar: "https://placehold.co/200",
+      },
+      {
+        contactName: "Nhà trường",
+        phoneNumber: "0988938139",
+        avatar: "https://placehold.co/200",
+      },
+      {
+        contactName: "GV: Võ Như Băng",
+        phoneNumber: "0375030543",
+        avatar: "https://placehold.co/200",
+      },
+      {
+        contactName: "GV: Phạm Thị Nhâm",
+        phoneNumber: "0375030543",
+        avatar: "https://placehold.co/200",
+      },
+      {
+        contactName: "GV",
+        phoneNumber: "0988938139",
+        avatar: "https://placehold.co/200",
+      },
+    ] 
+  },
+  {
+    contactCaterogy: "Hỗ trợ ứng dụng Starkid",
+    contactItems: [
+      {
+        contactName: "Zalo Chat StarKid",
+        phoneNumber: "0988938139",
+        avatar: "https://placehold.co/200",
+      },
+      {
+        contactName: "Zalo Chat StarKid",
+        phoneNumber: "0988938139",
+        avatar: "https://placehold.co/200",
+      },
+    ] 
+  }
+];

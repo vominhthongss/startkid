dc8153da4de798650eaf879235b7015f95caf234
vominhthongss <vominhthongss@gmail.com>
2024-05-06 15:07:13 +0900

    update LeaveRequestScreen.js

--- src/screens/LeaveRequest/LeaveRequestScreen.js
+++ src/screens/LeaveRequest/LeaveRequestScreen.js
@@ -1,9 +1,4 @@
-import {
-  View,
-  Text,
-  FlatList,
-  TouchableOpacity
-} from "react-native";
+import { View, Text, FlatList, TouchableOpacity } from "react-native";
 import * as SCREEN_NAMES from "../../constants/screensName";
 import { leaveRequests } from "../../mock/leaveRequests";
 import { useNavigation } from "@react-navigation/native";
@@ -16,18 +11,24 @@ function LeaveRequestScreen() {
 
   const handleGoToDetailScreen = (id) => {
     navigation.navigate(SCREEN_NAMES.leaveRequestDetail, { id: id });
-  }
+  };
 
   return (
     <View className="flex flex-row justify-center bg-white h-full">
       <FlatList
         data={leaveRequests}
-        keyExtractor={(item, index) => { index }}
-        renderItem={({ item }) => (
-          <LeaveRequestItem onSelected={() => { handleGoToDetailScreen(item.id) }} leaveRequestItem={item} />
-        )}>
-      </FlatList>
-      <TouchableOpacity className="absolute bottom-5 self-center px-4 py-2 rounded-3xl bg-[#0A6843]" onPress={() => { }}>
+        renderItem={({ item, index }) => (
+          <LeaveRequestItem
+            key={index}
+            onSelected={() => {
+              handleGoToDetailScreen(item.id);
+            }}
+            leaveRequestItem={item}
+          />
+        )}></FlatList>
+      <TouchableOpacity
+        className="absolute bottom-5 self-center px-4 py-2 rounded-3xl bg-[#0A6843]"
+        onPress={() => {}}>
         <View className="flex flex-row items-center">
           <SendIcon />
           <Text className="text-white ml-2">{STRINGS.createLeaveRq}</Text>

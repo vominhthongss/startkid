78ce4cc1aad1fc21b9ff07ba725dfebd733844c3
Trí Huy <trihuy2004@gmail.com>
2024-05-27 21:16:47 +0700

    change

--- assets/svg/DotIcon.js
+++ assets/svg/DotIcon.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Svg, { Path, Circle, Mask, G, Rect } from "react-native-svg";
 
-const DotIcon = ({ size, color }) => (
+const DotIcon = () => (
   <Svg
     width="10"
     height="10"

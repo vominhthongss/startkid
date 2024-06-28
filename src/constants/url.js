bbf0ccff01a639cd13f2602de72413a60ad582a0
vominhthongss <vominhthongss@gmail.com>
2024-06-28 22:06:12 +0900

    update url.js

--- src/constants/url.js
+++ src/constants/url.js
@@ -3,3 +3,4 @@ export const BASE_URL = "https://apis-ij6u.onrender.com";
 export const SIGN_IN = "/api/signin";
 export const HEALTH_POSTS = "/api/health-posts";
 export const CONTACT = "/api/contacts";
+export const NOTIFICATIONS = "/api/notifications";
